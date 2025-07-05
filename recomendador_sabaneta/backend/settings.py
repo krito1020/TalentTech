6c897b1f5156f7466eb2d72ee75d1c4a0e642e38
krito1020 <carolina.ospina152@gmail.com>
2025-07-04 22:54:25 -0500

    Actualización de proyecto TalentTech

--- recomendador_sabaneta/backend/settings.py
+++ recomendador_sabaneta/backend/settings.py
@@ -52,5 +52,8 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'apps/recomendador/static'),
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
