45343164f3bbe5584ddd3925c68e13bf56531916
Manuelita13 <manuelitalanubecita@gmail.com>
2025-07-13 16:18:26 -0500

    actiualice para subirlo en la nube

--- recomendador_sabaneta/backend/apps/recomendador/urls.py
+++ recomendador_sabaneta/backend/apps/recomendador/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='recomendador_home'),  # ← Este cambio es clave
+    path('', views.index, name='index'),  # ← Este cambio es clave
     path('registro/', views.registrar_comercio, name='registro'),
 ]
--- recomendador_sabaneta/backend/urls.py
+++ recomendador_sabaneta/backend/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import path
+from apps.recomendador import views
 
 urlpatterns = [
-    path('/', include('apps.recomendador.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  # Página principal
+    path('registro/', views.registrar_comercio, name='registro'),  # Página de registro
 ]
