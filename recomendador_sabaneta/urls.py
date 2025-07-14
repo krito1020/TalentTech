e46d5fefc679d2b469f81401c8d4f912ca1c4347
krito1020 <carolina.ospina152@gmail.com>
2025-07-14 09:19:10 -0500

    Delete recomendador_sabaneta directory

--- recomendador_sabaneta/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from apps.recomendador import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Página principal
-    path('registro/', views.registrar_comercio, name='registro'),  # Página de registro
-]
